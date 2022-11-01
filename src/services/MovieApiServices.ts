4a5599ec08948ffa343e1e3f2861048eee4283fb
Marijke Drenth <drenth.marijke@googlemail.com>
2022-11-01 12:03:58 +0100

    Change yaml workflow

--- src/services/MovieApiServices.ts
+++ src/services/MovieApiServices.ts
@@ -1,6 +1,7 @@
 import http from "./https-common";
-import { apikey } from "./apikey";
 import ApiRequestResult from "../types/ApiRequestResult"
+
+const apikey = process.env.MOVIEDB_API_KEY
 
 export const getPopularMovies = () => {
     return http.get<ApiRequestResult>(`/movie/popular?api_key=${apikey}&language=en-US&page=1`)
