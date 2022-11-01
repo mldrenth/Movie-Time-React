a68ca74b36d0701cd07150c2c0c3214a8e549391
Marijke Drenth <drenth.marijke@googlemail.com>
2022-11-01 11:23:34 +0100

    Change test

--- src/container/MovieDetailContainer.test.tsx
+++ src/container/MovieDetailContainer.test.tsx
@@ -2,6 +2,7 @@ import MovieDetailContainer from "./MovieDetailContainer";
 import { render, screen, waitFor } from "@testing-library/react"
 import { testMovie } from "../testfiles/TestMovie"
 
+
 describe("<MovieDetailContainer/>", () => {
     it("Should render title", async () => {
         render(<MovieDetailContainer movie={testMovie} />)
@@ -11,7 +12,9 @@ describe("<MovieDetailContainer/>", () => {
     it("Should render movie after fetch", async () => {
         render(<MovieDetailContainer movie={testMovie} />)
         let video: HTMLIFrameElement;
-        video = await screen.findByTitle("youtube-video")
-        await waitFor(() => expect(video).toBeInTheDocument())
+        
+        await waitFor(async () => {
+            video = await screen.findByTitle("youtube-video")
+            expect(video).toBeInTheDocument()})
     })
 })
